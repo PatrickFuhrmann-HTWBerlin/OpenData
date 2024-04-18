6ca4b30999f5bcb8ecb7d60213430903c024cd7c
Patrick Fuhrmann <patrick.fuhrmann@desy.de>
2024-04-18 13:54:10 +0200

    Improved search

--- src/people-search.sh
+++ src/people-search.sh
@@ -8,7 +8,13 @@ fi
 #
 search=$1
 #
-curl -s "http://localhost:3000/api/people?${search}" | jq .
+curl -w "\ncode=%{http_code}\n" \
+     -s \
+     "http://localhost:3000/api/people?${search}" |
+     awk -F= '/code/{ code=$2 } 
+             !/code/{ text=$0  }
+             END{  if( code == 200 )print text }' |
+     jq .
 #
 echo ""
 exit 0
