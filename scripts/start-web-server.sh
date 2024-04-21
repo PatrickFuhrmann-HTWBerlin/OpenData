12dd9fab288dba2192d8fe473e5139e5affc0b32
Patrick Fuhrmann <patrick.fuhrmann@desy.de>
2024-04-21 20:59:55 +0200

    More ports with variables

--- scripts/start-web-server.sh
+++ scripts/start-web-server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
 API_PORT=7003
+WEB_PORT=7002
 docker run -d \
        --name web-server \
-       --publish 7002:8300 \
+       --publish ${WEB_PORT}:${WEB_PORT} \
        --env OD_API_SERVER_HOST='api-server'  \
        --env OD_API_SERVER_PORT=${API_PORT}  \
-       --env OD_WEB_SERVER_PORT='7002'  \
+       --env OD_WEB_SERVER_PORT=${WEB_PORT}  \
        --network open-data-network \
        web-server
