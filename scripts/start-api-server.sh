ab5c6a908ef44a888aced389223a942a378c3cf7
Patrick Fuhrmann <patrick.fuhrmann@desy.de>
2024-04-21 20:37:54 +0200

    Using Variables for ports

--- scripts/start-api-server.sh
+++ scripts/start-api-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
+API_PORT=7003
+#
 docker run -d \
        --name api-server \
-       --publish 3000:3000 \
+       --publish ${API_PORT}:7003 \
        --env OD_CONFIG_MONGODB_SERVER=mongodb-server \
        --env OD_CONFIG_MONGODB_PORT=27017 \
        --env OD_CONFIG_MONGODB_ADMINUSERNAME=trude \
@@ -10,6 +12,6 @@ docker run -d \
        --env OD_CONFIG_MONGODB_DB_NAME=opendata \
        --env OD_CONFIG_MONGODB_COLLECTION_NAME=people \
        --env OD_CONFIG_SITE_BASEURL='/'  \
-       --env OD_CONFIG_PORT='3000'  \
+       --env OD_CONFIG_PORT=${API_PORT}  \
        --network open-data-network \
        api-server
--- scripts/start-web-server.sh
+++ scripts/start-web-server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
+API_PORT=7003
 docker run -d \
        --name web-server \
        --publish 7002:8300 \
        --env OD_API_SERVER_HOST='api-server'  \
-       --env OD_API_SERVER_PORT='3000'  \
+       --env OD_API_SERVER_PORT=${API_PORT}  \
        --env OD_WEB_SERVER_PORT='7002'  \
        --network open-data-network \
        web-server
