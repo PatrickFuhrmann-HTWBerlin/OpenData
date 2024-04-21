0668206d28f15482eab47664c9e868ba99c14ee2
Patrick Fuhrmann <patrick.fuhrmann@desy.de>
2024-04-21 08:45:18 +0200

    Better test and improved output

--- scripts/test-api-server.sh
+++ scripts/test-api-server.sh
@@ -5,12 +5,25 @@ first="Given-${rand}"
 last="Last-${rand}"
 dt="2000-01-01"
 
+echo "--- Adding Random user ${last} ${first} ${dt}"
+echo ""
 ./people-add.sh ${last} ${first} ${dt}
+echo ""
+echo "--- Adding same user again!"
+echo ""
 ./people-add.sh ${last} ${first} ${dt}
+echo ""
 
+echo "--- Searching for the user!"
+echo ""
 ./people-search.sh name=${last}
-
+echo ""
+echo "--- Removing the user again!"
+echo ""
 ./people-remove.sh ${last} ${first} ${dt}
+echo ""
+echo "--- Removing the same user!"
+echo ""
 ./people-remove.sh ${last} ${first} ${dt}
-
+echo ""
 exit 0
